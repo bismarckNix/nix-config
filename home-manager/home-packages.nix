44a2395cabf908cf04a345df909fc6863d2a33d8
bismarckNix <ludocryptwt@proton.me>
2026-09-13 20:11:19 +0300

    changed fetch from flake to nixpkgs

--- home-manager/home-packages.nix
+++ home-manager/home-packages.nix
@@ -6,11 +6,11 @@
     btop
     cava
     cmatrix
+    fetch
     lavat
     pipes
     tty-clock
     unimatrix
-    inputs.areofyl-fetch.packages.${pkgs.stdenv.hostPlatform.system}.default
 
     # Files
     (nemo-with-extensions.override {
