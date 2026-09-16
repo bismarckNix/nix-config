8c0bce3f5455fdd4ee677f158abb9287d2a3594d
bismarckNix <ludocryptwt@proton.me>
2026-09-16 15:33:00 +0300

    added templates

--- home-manager/modules/noctalia.nix
+++ home-manager/modules/noctalia.nix
@@ -518,7 +518,7 @@
         source = "wallpaper";
         wallpaper_scheme = "m3-rainbow";
         templates = {
-          builtin_ids = [ "btop" "kitty" ""];
+          builtin_ids = [ "btop" "kitty" "niri" "starship" "umbriel" ];
           community_ids = [ "discord" "telegram" "vscode" "steam" ];
         };
       };
