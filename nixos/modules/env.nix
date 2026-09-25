d4bcad2aa31bd17420b1520d0d6d5491cca3e955
bismarckNix <ludocryptwt@proton.me>
2026-09-25 17:41:19 +0300

    refactor: changed editor

--- nixos/modules/env.nix
+++ nixos/modules/env.nix
@@ -1,5 +1,7 @@
 {
   environment.sessionVariables = {
+    TERMINAL = "kitty";
+    EDITOR = "nvim";
     __NV_DISABLE_EXPLICIT_SYNC = "1";
     QSG_RHI_BACKEND = "vulkan";
     LD_LIBRARY_PATH = "/run/opengl-driver/lib";
