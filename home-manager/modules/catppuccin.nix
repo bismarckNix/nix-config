ad244247bfb63b7ba85cf7b6c4d542bbbdfa5685
bismarckNix <ludocryptwt@proton.me>
2026-09-25 23:13:33 +0300

    feat: added themes for some cli tool
    added: btop, cava, fzf

--- home-manager/modules/catppuccin.nix
+++ home-manager/modules/catppuccin.nix
@@ -3,10 +3,13 @@
     enable = true;
     autoEnable = false;
     flavor = "macchiato";
+    
     bat.enable = true;
+    btop.enable = true;
+    cava.enable = true;
     eza.enable = true;
+    fzf.enable = true;
     lazygit.enable = true;
-    vesktop.enable = true;
     zellij.enable = true;
     zsh-syntax-highlighting.enable = true;
   };
